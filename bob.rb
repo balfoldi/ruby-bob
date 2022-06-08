0901518e1ba4c2c05a002a29035d56c9329e12b0
balfoldi <boris@captaincontrat.com>
2022-06-08 20:31:33 +0200

    Bravo !

--- bob.rb
+++ bob.rb
@@ -5,8 +5,10 @@ class Bob
   def hey(remark)
     if /[a-zA-Z]+/.match?(remark) && remark.upcase == remark
       return 'Whoa, chill out!'
-    elsif /\?/.match?(remark)
+    elsif /\?$/.match?(remark.gsub("\n", ''))
       return 'Sure.'
+    elsif remark.strip.empty?
+      return 'Fine. Be that way!'
     end
 
     'Whatever.'
