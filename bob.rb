8c69e114b7e0a26796e93e5f12f9ced3f76cf9e5
balfoldi <boris@captaincontrat.com>
2022-06-08 19:59:44 +0200

    Pass more tests

--- bob.rb
+++ bob.rb
@@ -1,7 +1,14 @@
 class Bob
-    def initialize
+  def initialize
+  end
+
+  def hey(remark)
+    if remark.upcase == remark
+      return 'Whoa, chill out!'
+    elsif /\?/.match?(remark)
+      return 'Sure.'
     end
 
-    def hey(remark)
-    end
+    'Whatever.'
+  end
 end
