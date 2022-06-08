3dd33bf9643859daebe8c69f80508b158b397385
Matthieu de Goys <mdegoys@tuta.io>
2022-06-08 20:46:46 +0200

    refacto

--- bob.rb
+++ bob.rb
@@ -1,16 +1,12 @@
 class Bob
-  def initialize
-  end
-
   def hey(remark)
-    if /[a-zA-Z]+/.match?(remark) && remark.upcase == remark
+    if /[A-Z]+/.match?(remark) && remark.upcase == remark
       return 'Whoa, chill out!'
     elsif /\?$/.match?(remark.gsub("\n", ''))
       return 'Sure.'
     elsif remark.strip.empty?
       return 'Fine. Be that way!'
     end
-
     'Whatever.'
   end
 end
